903bcf483285ba7519206d6e4cf678ae64b4965d
thssnm <matthias.theissen@scoop-software.de>
2025-09-10 13:17:24 +0200

    add import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, JSX } from "react";
 
 // TypeScript Interfaces
 interface AdventText {
